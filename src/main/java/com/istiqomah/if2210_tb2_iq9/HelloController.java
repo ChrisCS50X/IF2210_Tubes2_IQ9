073c97957f5dc588fbe20ee77b0939b388b6f4cd
Christian Justin <114690452+ChrisCS50X@users.noreply.github.com>
2024-05-13 21:56:18 +0700

    initial commit

---
+++ src/main/java/com/istiqomah/if2210_tb2_iq9/HelloController.java
@@ -0,0 +1,14 @@
+package com.istiqomah.if2210_tb2_iq9;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.istiqomah.if2210_tb2_iq9 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.istiqomah.if2210_tb2_iq9 to javafx.fxml;
+    exports com.istiqomah.if2210_tb2_iq9;
+}
