0819e18c4a3059ef70e275ecceee3e44b36729bb
Christian Justin <christian.justin23@gmail.com>
2024-05-14 18:36:46 +0700

    Add Item Models

--- src/main/java/com/istiqomah/if2210_tb2_iq9/model/Delay.java
+++ src/main/java/com/istiqomah/if2210_tb2_iq9/model/Delay.java
@@ -1,4 +1,26 @@
 package com.istiqomah.if2210_tb2_iq9.model;
 
-public class Delay {
+public class Delay implements AnimalEffect, PlantEffect{
+    @Override
+    public void applyEffect(Animal animal) {
+        //
+        int beratAnimal = animal.getWeight();
+        if (beratAnimal <= 0) {
+            animal.setWeight(0);
+        }
+        else{
+            animal.setWeight(beratAnimal - 5);
+        };
+    }
+
+    @Override
+    public void applyEffect(Plant plant) {
+       int umurPlant = plant.getAge();
+       if (umurPlant <= 0) {
+              plant.setAge(0);
+       }
+       else{
+              plant.setAge(umurPlant - 2);
+       }
+    }
 }
--- src/main/java/com/istiqomah/if2210_tb2_iq9/model/InstantHarvest.java
+++ src/main/java/com/istiqomah/if2210_tb2_iq9/model/InstantHarvest.java
@@ -1,4 +1,13 @@
 package com.istiqomah.if2210_tb2_iq9.model;
 
-public class InstantHarvest {
+public class InstantHarvest implements AnimalEffect, PlantEffect {
+    @Override
+    public void applyEffect(Animal animal) {
+        animal.setWeight(animal.getHarvestWeight());
+    }
+
+    @Override
+    public void applyEffect(Plant plant) {
+        plant.setAge(plant.getHarvestDuration());
+    }
 }
