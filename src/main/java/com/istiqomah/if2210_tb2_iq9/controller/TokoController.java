e13ce6728ae3d8cde2d26c03bca8ab4e08b85a21
Christian Justin <christian.justin23@gmail.com>
2024-05-19 22:46:06 +0700

    Added folder controller

---
+++ src/main/java/com/istiqomah/if2210_tb2_iq9/controller/TokoController.java
@@ -0,0 +1,16 @@
+package com.istiqomah.if2210_tb2_iq9.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TokoController {
+    @FXML
+    private Label label;
+
+    @FXML
+    public void initialize() {
+        // Initialization logic here...
+    }
+
+    // Add methods for handling user interactions here...
+}
